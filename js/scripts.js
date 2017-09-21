5f1bc1767a0b79602100c25d2ad2952063877fc5
Alan Falcon and Michael Arrington <alan+michael@>
2017-09-21 16:02:45 -0700

    change input from type of text to type of number and add a placeholder as well.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function integer_to_roman(n) {
   decimal = [1,   4,   5,   9,  10,  40,  50,  90, 100, 400, 500, 900,1000];
   roman = ["I","IV", "V","IX", "X","XL", "L","XC", "C","CD", "D","CM", "M"];
   for (i = 12; i >= 0; i--) {
-      
+
       while (n >= decimal[i]) {
         console.log(r,n);
           r += roman[i];
@@ -32,6 +32,7 @@ $(document).ready(function() {
     // alert("this one");
     var userInput = $("input#userInput").val();
     userInput = parseInt(userInput.replace(/,/g,""));
+    //fix logic below to not allow a space such as "200 90" and ensure it is a single integer
     if ((Number.isInteger(userInput) === false) || (Math.sign(userInput) !== 1) || (userInput > 3999)) {
       alert("Please enter a valid number 1-3999");
     } else $("#output").text(integer_to_roman(userInput));
